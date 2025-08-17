c93b1bea6d7be24152d41490a3da09431ecc5a78
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-17 20:04:56 -0300

    fe: add NotionChildView route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NotionDataViewer from '@/views/NotionParentView.vue'
+import NotionChildView from '@/views/NotionChildView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: NotionDataViewer,
+    },
+    {
+      path: '/block/:block_id',
+      name: 'block',
+      component: NotionChildView,
     },
   ],
 })
---
+++ frontend/src/views/NotionChildView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Block</div>
+</template>
