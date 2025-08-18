48d9d869ddf29b186773c2e1e6c1412d31d8e572
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-18 14:12:58 -0300

    fe: add Notion icon to App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,22 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-  </header>
-  <section>
-    <div class="">
-      <RouterView />
-    </div>
-  </section>
+  <v-app width="100%">
+    <v-container width="100%">
+      <v-row class="justify-center">
+        <header>
+          <img alt="Notion logo" class="logo" src="@/assets/notion_icon.svg" width="125" height="125" />
+        </header>
+      </v-row>
+      <v-row>
+        <section>
+          <div class="">
+            <RouterView />
+          </div>
+        </section>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <style></style>
