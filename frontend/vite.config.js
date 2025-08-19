4245fa5ff84d072a10aa85fd5969f4d319d9a0b7
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-19 19:40:46 -0300

    fe: break node deps up into chunks for build. Gen report.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -27,4 +27,34 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vuetify/lib')) {
+              if (id.includes('components')) {
+                return 'vendor-vuetify-components';
+              } else if (id.includes('composables')) {
+                return 'vendor-vuetify-composables';
+              }
+
+              return 'vendor-vuetify-other';
+            }
+            if (id.includes('@vue')) {
+              return 'vendor-vue';
+            }
+            if (id.includes('axios')) {
+              return 'vendor-axios';
+            }
+            if (id.includes('vue3-toastify/dist')) {
+              return 'vendor-toastify';
+            }
+
+            return 'vendor-other';
+          }
+        }
+      }
+    }
+  }
 })
