d75daf601166ca7eee963968bdf899e4657182da
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-19 12:31:36 -0300

    be: return title from page properties in blocks endpoint

--- backend/controllers/notionDataController.js
+++ backend/controllers/notionDataController.js
@@ -51,15 +51,33 @@ const formatBlockChildrenData = (data) => {
 // List Block Children of Page
 export const getBlockChildren = async (req, res) => {
   try {
+    const pageTitle = await getPageTitle(req.params.block_id)
     const response = await notion.blocks.children.list({
       block_id: req.params.block_id,
     });
-    const formatedData = formatBlockChildrenData(response.results);
+    const formatedData = {
+      title: pageTitle,
+      blocks: formatBlockChildrenData(response.results)
+    };
     res.status(200).json(formatedData);
   } catch (error) {
     console.error('apiError ==>> ', error);
     res.status(500).json({ apiError: error });
   };
+};
+
+// Get pages properties
+const getPageTitle = async (pageId) => {
+  try {
+    const response = await notion.pages.properties.retrieve({
+      page_id: pageId,
+      property_id: "title",
+    });
+
+    return response.results[0]?.title?.plain_text;
+  } catch (error) {
+    console.error('apiError ==>> ', error);
+  }
 };
 
 // List Notion Users
