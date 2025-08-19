18112cda1d3c8b85b54cbfd1b2ff2eb1a23c70d4
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-19 19:53:58 -0300

    fe:fix chunk build fail. Gen report.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -32,14 +32,8 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('vuetify/lib')) {
-              if (id.includes('components')) {
-                return 'vendor-vuetify-components';
-              } else if (id.includes('composables')) {
-                return 'vendor-vuetify-composables';
-              }
-
-              return 'vendor-vuetify-other';
+            if (id.includes('vuetify')) {
+              return 'vendor-vuetify';
             }
             if (id.includes('@vue')) {
               return 'vendor-vue';
