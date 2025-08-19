87105d00f3a3a9d24fe74628567377688cf55dbb
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-19 19:25:27 -0300

    fe:add visualizer to vite config. Gen report.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    visualizer({
+      open: true,
+      filename: "bundle-report.html",
+    }),
   ],
   css: {
     preprocessorOptions: {
