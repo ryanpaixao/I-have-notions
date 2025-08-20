250310cd00c372af3d1cec5ecd497b8f866aebd2
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-20 14:41:57 -0300

    be:rm block route from notionDataRoutes

--- backend/routes/notionDataRoutes.js
+++ backend/routes/notionDataRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { getNotionData } from '../controllers/notionDataController.js';
-import { getBlockChildren } from '../controllers/notionBlockController.js';
 
 const router = express.Router();
 
 router.get('/query-data/:database_id', getNotionData);
-router.get('/block/:block_id', getBlockChildren);
 
 export default router;
