217ad95c7d3ccc40f7d7fe20ee615e057a7efc3f
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-20 12:28:12 -0300

    fe:add GlossaryView and NotFoundView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import NotionDataViewer from '@/views/NotionParentView.vue'
 import NotionChildView from '@/views/NotionChildView.vue'
 
+import GlossaryView from '@/views/GlossaryView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,9 +14,19 @@ const router = createRouter({
       component: NotionDataViewer,
     },
     {
+      path: '/glossary',
+      name: 'glossary',
+      component: GlossaryView,
+    },
+    {
       path: '/block/:block_id',
       name: 'block',
       component: NotionChildView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     },
   ],
 })
