5c2d87ff2fde2c9fa2507f60a537b46e2e4c3e99
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-26 14:30:53 -0300

    be:rm console.logs

--- backend/utils/page.js
+++ backend/utils/page.js
@@ -2,15 +2,13 @@ export const formatPageData = (data) => {
   const formatedRichText = [];
   const dataIsObject = data !== null && typeof data === 'object';
   const pageTitle = dataIsObject && data.Nome?.title?.[0]?.plain_text;
-  console.log('data.Nome ==>>', data.Nome);
+
   if (dataIsObject) {
     Object.keys(data).forEach((key) => {
-      // console.log('key ==>>', key);
       const richText = data[key].rich_text
       const hasRichText = richText && Array.isArray(richText);
 
       hasRichText && richText.forEach(text => {
-        // console.log(`${key} ==>>`, data[key])
         text.plain_text && formatedRichText.push([key, text.plain_text]);
       });
     });
