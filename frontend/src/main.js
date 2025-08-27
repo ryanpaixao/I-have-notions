a75e0cd5ba75ee278fd3ba76eea62da6739bd737
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-27 14:25:36 -0300

    fe:add treeshaking vite plugin for vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,13 +9,9 @@ import router from './router';
 // Styling
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import '@mdi/font/css/materialdesignicons.css';
 
 const vuetify = createVuetify({
-  components,
-  directives,
   theme: {
     defaultTheme: 'system',
   },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify(),
     vueDevTools(),
     visualizer({
       open: true,
