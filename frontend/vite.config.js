7344ff91d3d82f9b22f0d3b5c7343c721f1d4667
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-27 14:14:10 -0300

    fe:update vite config server to allow just lan

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,6 +22,17 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
